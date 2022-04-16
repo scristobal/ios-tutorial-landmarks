e53c06be58cf83b0a464808c64370a97b325800d
Samuel Cristobal <el.tobal@gmail.com>
2022-04-16 20:02:26 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Samuel Cristobal on 16/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
